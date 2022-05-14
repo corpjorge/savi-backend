fd0fe225b34830b19838e796789bd8db7d45b58c
Jorge Eduardo <corpjorge@hotmail.com>
2022-05-13 19:35:47 -0500

    feat: meet validation create

--- app/Http/Controllers/Meeting/StoreMeetingController.php
+++ app/Http/Controllers/Meeting/StoreMeetingController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Meeting;
 
 use App\Models\Meetings;
+use Carbon\Carbon;
 use Illuminate\Validation\ValidationException;
 
 class StoreMeetingController
@@ -22,19 +23,38 @@ class StoreMeetingController
         $meeting->state = 'active';
         $meeting->meeting = \Illuminate\Support\Str::random(7);
         $meeting->save();
+
     }
 
     public function validations($request)
     {
-        if($this->activeMeetings()) {
+        if ($this->activeMeetings()) {
             throw ValidationException::withMessages([
                 'error' => ['A meeting already exists for this user.'],
             ]);
         }
 
-        if($this->validateDate($request->date)) {
+        if ($this->validateDate($request->date)) {
             throw ValidationException::withMessages([
                 'error' => ['The date is not available.'],
+            ]);
+        }
+
+        if ($this->validateSunday($request->date)) {
+            throw ValidationException::withMessages([
+                'error' => ['The date is not available.'],
+            ]);
+        }
+
+        if ($this->validateSaturday($request->date)) {
+            throw ValidationException::withMessages([
+                'error' => ['The time is not available.'],
+            ]);
+        }
+
+        if ($this->validateTime($request->date)) {
+            throw ValidationException::withMessages([
+                'error' => ['The time is not available.'],
             ]);
         }
 
@@ -45,14 +65,27 @@ class StoreMeetingController
         return Meetings::where('user_id', auth()->id())->where('state', 'active')->exists();
     }
 
-    public function validateDate($date)
+    public function validateDate($date): bool
     {
-         return date("Y-m-d") > $date;
+        return date("Y-m-d") > $date;
     }
 
+    public function validateSunday($date): bool
+    {
+        return Carbon::create($date)->isoFormat('E') == 7;
+    }
 
+    public function validateSaturday($date): bool
+    {
+        $date = Carbon::create($date);
+        return $date->isoFormat('E') == 6 && $date->hour >= 12;
+    }
 
-
+    public function validateTime($date): bool
+    {
+        $date = Carbon::create($date);
+        return $date->hour > 17 || $date->hour < 8;
+    }
 
 
 }
