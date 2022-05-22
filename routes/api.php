a8734f6cb73ede13ee5154701e40e33b84d12593
Jorge Eduardo <corpjorge@hotmail.com>
2022-05-21 21:51:40 -0500

    ajuste url verificacion mail

--- routes/api.php
+++ routes/api.php
@@ -14,5 +14,14 @@ require_once 'application.php';
 
 Route::group(['middleware' => 'auth:sanctum'], function () {
     Route::post('/logout', App\Http\Controllers\Authentication\LogoutController::class);
-    Route::get('/user', App\Http\Controllers\Authentication\AuthenticatedUserController::class);
+    Route::get('/user', App\Http\Controllers\Authentication\AuthenticatedUserController::class)->middleware('verified');
+
+    Route::get('/email/verify/{id}/{hash}', function (\Illuminate\Foundation\Auth\EmailVerificationRequest $request) {
+        $request->fulfill();
+    })->middleware('signed')->name('verification.verify');
+
+    Route::post('/email/verification-notification', function (\Illuminate\Http\Request $request) {
+        $request->user()->sendEmailVerificationNotification();
+    })->middleware('throttle:6,1')->name('verification.send');
+
 });
